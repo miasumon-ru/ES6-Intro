0b3a698647ac8577dc2fe79ee5c6de65cd6510d5
miasumon <miasumon.ru@gmail.com>
2024-02-22 17:46:32 +0600

    template es6

---
+++ template.js
@@ -0,0 +1,32 @@
+const first = 'jahan';
+const last = 'pakhi';
+
+const name = first + ' ' + last;
+// console.log(name);
+
+// using backtick
+
+console.log(` ${first} ${last}`)
+
+
+const a  = 10;
+const b = 20;
+
+// console.log(` the sum of a = ${a} and b = ${b} is ${a + b}`);
+
+// new line with backtick
+
+const email = ` hi john
+cena cena lage 
+aso maramari kori`;
+console.log(email);
+
+
+const numbers  = [15,48,65,25];
+
+const student = {
+    name: 'ceena',
+    age : 26
+}
+
+console.log(` the sum of ${numbers[0]} and ${student.age} is ${numbers[0] + student.age}`)
